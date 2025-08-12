4ebc5f03ba760684c917192109d52b3a619ba210
Van Nguyen <nhvanse@gmail.com>
2025-08-12 08:28:26 +0700

    add example ec2 instance_11

---
+++ 11-CI-CD-terraform-zero-downtime/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+  s3_use_path_style = true # Required for LocalStack
+}
+
+data "aws_ami" "ami" {
+    most_recent = true
+    owners      = ["099720109477"] # Canonical
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    }
+}
+
+resource "aws_instance" "instance_11" {
+    ami           = data.aws_ami.ami.id
+    instance_type = "t2.micro"
+    tags = {
+        Name        = "instance_11"
+    }
+}
